434356843a68cc46ae344f946cbfa3620fe5a361
edpowley <edward.powley@falmouth.ac.uk>
2017-06-16 15:19:53 +0100

    Testing matrix

--- test_wsA.py
+++ test_wsA.py
@@ -1,15 +1,17 @@
-def test_two_plus_two():
-    assert 2 + 2 == 4
+import os
 
-def test2():
-    assert 2 + 2 == 4
+if os.environ.get('STRETCH') == 'true':
+	def test3():
+		assert 2 + 2 == 5
 
-def test3():
-    assert 2 + 2 == 5
+	def test4():
+		assert 2 + 2 == 4
 
-def test4():
-    assert 2 + 2 == 4
+	def test5():
+		assert 2 + 2 == 6
+else:
+	def test_two_plus_two():
+		assert 2 + 2 == 4
 
-def test5():
-    assert 2 + 2 == 6
-
+	def test2():
+		assert 2 + 2 == 4
--- test_wsA_2.py
+++
@@ -1,2 +0,0 @@
-def test_3():
-    assert 3 + 3 == 6
