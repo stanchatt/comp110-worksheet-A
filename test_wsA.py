909d883bdcd71969bf19ac02aca864f5d7b6820b
edpowley <edward.powley@falmouth.ac.uk>
2017-06-16 15:03:56 +0100

    More tests

--- test_wsA.py
+++ test_wsA.py
@@ -1,3 +1,15 @@
 def test_two_plus_two():
     assert 2 + 2 == 4
 
+def test2():
+    assert 2 + 2 == 4
+
+def test3():
+    assert 2 + 2 == 5
+
+def test4():
+    assert 2 + 2 == 4
+
+def test5():
+    assert 2 + 2 == 6
+
